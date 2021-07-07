1f25027c02c19cc0512cf09d3d5d150b2d0e1c83
abdulrahman-at-ibm <abdulrahman.yussuf.ibm@gmail.com>
2021-07-07 19:04:44 +0100

    Add chat feature

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,26 @@
+import consumer from "./consumer"
+
+const room = consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+		console.log("Connected to chat room.")
+		$('.messages').scrollTop($('.messages')[0].scrollHeight)
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+				console.log("Disconnected from chat room.")
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+		$('.messages').append(`<div class="message"><p class="content">${data.content}</p></div>`)
+		$('.messages').scrollTop($('.messages')[0].scrollHeight)
+  }
+});
+
+$('form').submit(event => {
+	room.send({content: event.target.value})
+	$('#message_content').val('')
+	event.preventDefault()
+})
